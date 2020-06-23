c2425c7da83197e7815ba6f4ebfd9801b21cfbec
Divya Singla <divya.singla@storehub.com>
2020-06-23 11:12:39 +0800

    Final changes for customer test cases

--- debugtalk.py
+++ debugtalk.py
@@ -2,6 +2,7 @@
 import time
 import random
 import uuid
+from datetime import datetime
 
 
 def sleep(n_secs):
@@ -33,4 +34,10 @@ def create_uuid_15():
     ssuid = suid[0:15]
     return ssuid
 
+
+def get_unique_refid():
+    return str(uuid.uuid4())
+
+def get_datetime():
+    return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
