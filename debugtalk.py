d649af9d66d1f5f9af543f62667e33c4fe15df33
Venky Xu <venky.xu@storehub.com>
2020-06-23 14:36:51 +0800

    no message

--- debugtalk.py
+++ debugtalk.py
@@ -38,6 +38,7 @@ def create_uuid_15():
 def get_unique_refid():
     return str(uuid.uuid4())
 
+
 def get_datetime():
-    return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    return str(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
 
