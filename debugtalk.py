3ffe6ce4d6870f78152701cb44131256b9a0cc9b
Venky Xu <venky.xu@storehub.com>
2020-10-19 18:16:46 +0800

    Beep core working flow

--- debugtalk.py
+++ debugtalk.py
@@ -1,8 +1,7 @@
-
 import time
 import random
 import uuid
-from datetime import datetime
+import datetime
 
 
 def sleep(n_secs):
@@ -24,7 +23,7 @@ def storename():
 
 
 def mail():
-    email = storename()+'@abc.com'
+    email = storename() + '@abc.com'
     return email
 
 
@@ -41,7 +40,7 @@ def uuid_32():
 
 
 def create_sku():
-    sku= "sku" + str(random.randint(500,100000))
+    sku = "sku" + str(random.randint(500, 100000))
     return sku
 
 
@@ -50,10 +49,46 @@ def get_unique_refid():
 
 
 def get_datetime():
-    return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    return datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+
+def get_tomorrow():
+    return (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%dT%H:00:00.000Z")
 
 
 def create_receipt_number():
-    return "001"+datetime.now().strftime("%y%m%d%H%M%S")+str(random.randint(1, 9))
+    return "001" + datetime.datetime.now().strftime("%y%m%d%H%M%S") + str(random.randint(1, 9))
 
+
+def set_cookie__s(cookie, arg):
+    cookie["__s"] = arg
+    a = []
+    c = ""
+    for key in cookie:
+        a.append(key + "=" + cookie[key])
+    for i in a:
+        c = c + i + ";"
+    return c[0:len(c) - 1]
+
+
+def set_cookie_sid(arg1, arg2):
+    cookie = {"__s": arg2, "sid": arg1}
+    a = []
+    c = ""
+    for key in cookie:
+        a.append(key + "=" + cookie[key])
+    for i in a:
+        c = c + i + ";"
+    return c[0:len(c) - 1]
+
+
+def set_cookie__t(arg1, arg2, arg3):
+    cookie = {"__s": arg2, "sid": arg1, "__t": arg3}
+    a = []
+    c = ""
+    for key in cookie:
+        a.append(key + "=" + cookie[key])
+    for i in a:
+        c = c + i + ";"
+    return c[0:len(c) - 1]
 
